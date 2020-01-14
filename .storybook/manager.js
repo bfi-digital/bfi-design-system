72073547970450d278a0ccd7417bdbaec3322c4c
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-01-14 14:12:43 +0000

    add netlify config

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -4,7 +4,6 @@ import { create } from "@storybook/theming/create"
 addons.setConfig({
   theme: create({
     base: "dark",
-  
     brandTitle: 'British Film Institute',
     brandUrl: 'https://bfi.org.uk',
   //   brandImage: 'https://placehold.it/350x150',
