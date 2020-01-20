ef83f26c7c31e64e6e4f5bd98410520111c04c42
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-01-20 12:13:56 +0000

    add theme file

---
+++ src/_theme.js
@@ -0,0 +1,28 @@
+export default {
+
+    // colour scheme
+    charcoal: "#1c1c1c",
+    white: "#ffffff",
+
+    darkGrey: "#746D6D",
+    grey: "#e8e5e5",
+    lightGrey: "f6f6f6",
+
+    lightPink: "#ffe6e7",
+    dustyPink: "#f0c5c8",
+    darkPink: "#895d60",
+
+    lightBlue: "#c1d6f5",
+    deepNavy: "#031d45",
+    darkBlue: "#1a2029",
+
+    lightGreen: "#c0edd9",
+    racerGreen: "#004225",
+    darkGreen: "#172820",
+
+    // breakpoints
+    m: "768px", // tablets and larger
+    l: "960px", // desktops and larger
+    xl: "1440px" // large desktops only
+
+}
