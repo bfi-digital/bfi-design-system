cbdc2e48603e08f7488429838d1d133eb43cac8a
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-01-21 16:07:10 +0000

    work on header + update snapshots

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,79 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "@reach/router"
+import styled from "styled-components"
+import theme from "../_theme"
+
+import logo from "./logo-black.svg"
+import menuIcon from "./menu-black.svg"
+import closeIcon from "./close-black.svg"
+
+const Outer = styled.header`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+`
+
+const MenuButton = styled.button`
+    border: none;
+    cursor: pointer;
+    background: none;
+    padding: 10px;
+    font-size: 1rem;
+    font-weight: bold;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    color: ${theme.charcoal};
+    img{
+        height: 12px;
+        margin-right: 5px;
+    }
+`
+
+const Logo = styled.img`
+    width: 46px;
+`
 
 export const Header = ({
     navItems
-}) =>
-    <header>
-        {navItems.map((navItem, i) =>
-            <li key={i}>
-                <Link to={navItem.url}>
-                    {navItem.title}
+}) => {
+
+    const [mobilePanel, setMobilePanel] = useState(false)
+
+    return(
+        <>
+            <Outer>
+                <Link to="/">
+                    <Logo src={logo} alt="British Film Institute"/>
                 </Link>
-            </li>
-        )}
-    </header>
+                <MenuButton onClick={() => setMobilePanel(!mobilePanel)}>
+                    {mobilePanel ? 
+                        <>
+                            <img src={closeIcon} alt=""/>
+                            Close
+                        </>
+                        :
+                        <>
+                            <img src={menuIcon} alt=""/>
+                            Menu
+                        </>
+                    }
+                </MenuButton>    
+            </Outer>
+
+            {mobilePanel && navItems.map((navItem, i) =>
+                <li key={i}>
+                    <Link to={navItem.url}>
+                        {navItem.title}
+                    </Link>
+                </li>
+            )}
+
+
+        </>
+    )
+}
 
