35b60652f9fe7d6b40a72579bcb455a779ab2ffd
Chris Evans <evanschris@users.noreply.github.com>
2020-02-21 19:55:23 +0000

    opps

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -63,6 +63,7 @@ export const Headline = ({
     level,
     text
 }) => {
+    if (level === 0) return <HeroH1>{text}</HeroH1>
     if (level === 1) return <H1>{text}</H1>
     if (level === 2) return <H2>{text}</H2>
     if (level === 3) return <H3>{text}</H3>
