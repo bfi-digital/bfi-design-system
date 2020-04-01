2f4686f9dc699a6004a7b6fc9577b96001344b95
Chris Evans <evanschris@users.noreply.github.com>
2020-04-01 14:59:29 +0100

    add call to action slice

---
+++ src/components/CallToAction/index.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import styled from "styled-components"
+import PropTypes from "prop-types"
+import { Button } from "../Button"
+
+export const Outer = styled.div`
+    margin-bottom: 20px;
+`
+
+export const CallToAction = ({
+    linkText,
+    linkURL,
+    external
+}) =>
+    <Outer>
+        <Button to={linkURL} external={external}>
+            {linkText}
+        </Button>
+    </Outer>
+
+
+CallToAction.propTypes = {
+    /** 
+	 * The text of the button
+	 **/
+    linkText: PropTypes.string,
+    /** 
+	 * The URL the button links to
+	 **/
+    citation: PropTypes.string,
+    external: PropTypes.bool
+}
