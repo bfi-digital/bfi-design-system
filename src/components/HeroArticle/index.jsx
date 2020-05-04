e026a4287b9d6150bd4e11732fda24653cb50814
Chris Evans <evanschris@users.noreply.github.com>
2020-05-04 11:39:07 +0100

    add article hero and an example of an article post

---
+++ src/components/HeroArticle/index.jsx
@@ -0,0 +1,90 @@
+import React from "react"
+import styled from "styled-components"
+import theme from "../_theme"
+import PropTypes from "prop-types"
+import { Headline } from "../Headline"
+import { LeadParagraph } from "../LeadParagraph"
+import { Tag } from "../Tag"
+import { Image } from "../Image"
+
+
+const Outer = styled.div`
+    margin: 0 auto;
+    text-align: center;
+    margin-top: 50px;
+    max-width: ${theme.xl};
+    max-width: calc(${theme.xl} + 125px);
+`
+const Meta = styled.div`
+    max-width: 800px;
+    padding: 0 50px;
+    margin: 0 auto; 
+`
+const LowerContent = styled.div`
+    margin: 30px auto;
+    max-width: 400px;
+    p {
+        margin-top: 0;
+    }
+`
+export const HeroArticle = ({
+    image1920x1080,
+    imageAltText,
+    imageCopyright,
+    title,
+    standfirst,
+    category,
+    type,
+    date,
+    authors
+}) =>
+    <Outer>
+        <Meta>
+            {category && <Tag>{category}</Tag>}
+            {type && <Tag>{type}</Tag>}
+            {title && <Headline level={1} text={title}/>}
+            {standfirst && <LeadParagraph text={standfirst}/>}
+            <LowerContent>
+                {date && <span>{date}</span>}
+                {authors.length === 1 ?
+                    <span> by: <a href={authors[0].url}>{authors[0].name}</a></span>
+                    :
+                    <p>
+                        By:&nbsp;
+                        {authors.map((author, index) =>
+                            <><a key={`author_${index}`} href={author.url}>{author.name}</a>{(index < (authors.length-1) ? ", " : "")}</>
+                        )}
+                    </p>
+                }
+            </LowerContent>
+        </Meta>
+        {image1920x1080 &&
+            <Image
+                alt={imageAltText}
+                src={image1920x1080}
+                copyright={imageCopyright}
+            />
+        }
+    </Outer>
+
+
+HeroArticle.propTypes = {
+    // Urls to the image for the article hero. 
+    image1920x1080: PropTypes.string,
+    // Alt text for hero image.
+    imageAltText: PropTypes.string,
+    // Optional copyright text for the hero image.
+    imageCopyright: PropTypes.string,
+    // The text for the title which will be used as the H1 for this page/post
+    title: PropTypes.string,
+    // A summary of the article.
+    standfirst: PropTypes.string,
+    // The category of the article.
+    category: PropTypes.string,
+    // The optional type tag of the article.
+    type: PropTypes.string,
+    // The date the article was posted.
+    date: PropTypes.string,
+    // An array of the authors of this article
+    authors: PropTypes.array,
+}
