d079ff4380358430e2591b036113caf42e7dfa60
Chris Evans <evanschris@users.noreply.github.com>
2020-05-11 13:55:51 +0100

    fix categories in articles

--- src/components/FilterableArticles/index.jsx
+++ src/components/FilterableArticles/index.jsx
@@ -36,7 +36,7 @@ const transformArticles = articles => articles.map(article => {
         title: article.title,
         url: article.url,
         author: article.authors ? transformAuthors(article.authors) : false,
-        categories: article.category ? [article.category.name] : false,
+        category: article.category ? article.category.name : "Uncategorised",
         image480x270: article.primary_image[2].url,
         date: moment(article.created).format("dddd Do MMMM YYYY")
     }
