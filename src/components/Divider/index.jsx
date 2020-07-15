7ae624f74c6c59799c360c2aa390a025734de1b1
Chris Evans <evanschris@users.noreply.github.com>
2020-07-15 18:01:14 +0100

    update divider

--- src/components/Divider/index.jsx
+++ src/components/Divider/index.jsx
@@ -7,7 +7,7 @@ const Break = styled.hr`
     border: 1px solid ${theme.darkGrey};
     margin: ${props => props.istransparent ? (theme.standardSpace + "px") : ((theme.standardSpace*2) + "px")} auto;
     width: 100%;
-    opacity: ${props => props.istransparent ? "0" : "0.5"};
+    opacity: ${props => props.istransparent ? "0" : "0.15"};
 
     @media screen and (min-width: ${theme.m}){
         margin: ${props => props.istransparent ? ((theme.standardSpace*2) + "px") : ((theme.standardSpace*3) + "px")} auto;
