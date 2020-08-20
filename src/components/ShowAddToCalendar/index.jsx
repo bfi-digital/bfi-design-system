45766a04ed023651c975bf59f4557992217b50d2
Chris Evans <evanschris@users.noreply.github.com>
2020-08-20 15:19:39 +0100

    style calendar button

--- src/components/ShowAddToCalendar/index.jsx
+++ src/components/ShowAddToCalendar/index.jsx
@@ -3,7 +3,9 @@ import styled from "styled-components"
 import theme from "../_theme"
 import AddToCalendar from "react-add-to-calendar"
 
-const Outer = styled.p`
+const Outer = styled.div`
+    margin-top: 15px;
+    @import url("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
     background: transparent;
     color: ${theme.primary};
     font-weight: bold;
@@ -11,8 +13,44 @@ const Outer = styled.p`
     font-size: 1.2rem;
     padding: 0;
     cursor: pointer;
-`
 
+    ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+    li {
+        padding-left: 0;
+        a {
+            color: ${theme.primary};
+            display: block;
+            padding: 7px 0px;
+        }
+        a:hover {
+            text-decoration: none;
+        }
+        i {
+            margin-right: 5px;
+        }
+    }
+
+    .react-add-to-calendar__dropdown {
+        background: ${theme.lightest};
+        padding: 15px 25px;
+        margin-left: -5px;
+    }
+    .react-add-to-calendar__button {
+        display: block;
+        border: 3px solid transparent;
+        margin-left: -5px;
+        padding-left: 2px;
+
+        &.open {
+            border: 3px solid ${theme.lightest};
+            color: ${theme.dark};
+        }
+    }
+`
 
 export const ShowAddToCalendar = ({
     title,
@@ -28,7 +66,7 @@ export const ShowAddToCalendar = ({
         startTime: dateTimeStart,
         endTime: dateTimeEnd
     }
-    let calendarIcon = { "calendar-plus-o": "left" }
+    let icon = { "caret": "right" }
     let calendarItems = [
         { apple: "Apple Calendar" },
         { google: "Google" },
@@ -38,7 +76,14 @@ export const ShowAddToCalendar = ({
     return(
         <Outer>
             {/* NEED TO UPDATE THIS TO MAKE IT ACCESSIBLE, MAYBE LOOK AT http://leibowitz.me/react-add-to-calendar-hoc/ */}
-            <AddToCalendar event={calendarEvent} listItems={calendarItems} buttonTemplate={calendarIcon} displayItemIcons={true} />
+            <i class="react-add-to-calendar__icon--left fa fa-calendar-plus-o"></i>
+            <AddToCalendar 
+                event={calendarEvent} 
+                listItems={calendarItems} 
+                buttonTemplate={icon} 
+                displayItemIcons={true} 
+                buttonClassOpen="open"
+            />
         </Outer>
     )
 }
