660518d2c47d4d47a111ea30b09e87a26f6cca40
Jack Holding <jack.holding@manifesto.co.uk>
2021-05-13 10:39:44 +0100

    D20-1088 – reduce margin for the `Divider` component

--- src/components/Divider/index.jsx
+++ src/components/Divider/index.jsx
@@ -5,12 +5,12 @@ import theme from "../_theme"
 
 const Break = styled.hr`
     border: 1px solid ${theme.darkGrey};
-    margin: ${props => props.istransparent ? (theme.standardSpace + "px") : ((theme.standardSpace*2) + "px")} auto;
+    margin: ${props => props.istransparent ? ((theme.standardSpace*0.5) + "px") : (theme.standardSpace + "px")} auto;
     width: 100%;
     opacity: ${props => props.istransparent ? "0" : "0.15"};
 
     @media screen and (min-width: ${theme.m}){
-        margin: ${props => props.istransparent ? ((theme.standardSpace*2) + "px") : ((theme.standardSpace*3) + "px")} auto;
+        margin: ${props => props.istransparent ? ((theme.standardSpace*0.75) + "px") : ((theme.standardSpace*1.5) + "px")} auto;
     }
 `
 export const Divider = ({
