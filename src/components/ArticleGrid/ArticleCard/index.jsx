1d729fd7dc3cb435c93570f418fc76b21f9363f2
Jack Holding <jack.holding@manifesto.co.uk>
2021-06-16 15:02:45 +0100

    D20-1080 – fix `ArticleCard` component a11y errors

--- src/components/ArticleGrid/ArticleCard/index.jsx
+++ src/components/ArticleGrid/ArticleCard/index.jsx
@@ -180,7 +180,7 @@ export const ArticleCard = ({
                 src={image480x270}
                 placeholder={image48x27 ? image48x27 : image480x270}
             >
-                {src => <PageImage className="image" imageSrc={src} alt="" />}
+                {src => <PageImage className="image" imageSrc={src} />}
             </LazyImage>
         </PageImageContainer>
         <Headline level={6} text={title} />
