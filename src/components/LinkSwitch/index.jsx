afd0367b667eafc55783be31559364e0ec0bad8a
Jack Holding <jack.holding@manifesto.co.uk>
2021-06-17 11:22:59 +0100

    D20-1080 – fix `LinkSwitch` component a11y errors

--- src/components/LinkSwitch/index.jsx
+++ src/components/LinkSwitch/index.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 
-export const LinkSwitch = ({
-    url,
-    to,
-    children,
-    ...props
-}) => 
-    <a href={url || to} {...props}>
-        {children}
-    </a>
+export const LinkSwitch = ({ url, to, children, ...props }) => {
+    const linkProps = Object.assign({}, props)
+    // Remove invalid attributes from html output.
+    delete linkProps.color
+    delete linkProps.colorScheme
+    delete linkProps.strandColorScheme
+
+    return (
+        <a href={url || to} {...linkProps}>
+            {children}
+        </a>
+    )
+}
