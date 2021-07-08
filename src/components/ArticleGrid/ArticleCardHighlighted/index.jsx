d703f67b5e2ad10e69c7ae3743a848a6e440818b
Jack Holding <jack.holding@manifesto.co.uk>
2021-07-08 11:38:02 +0100

    D20-802 – safari a11y tabbing fix for the component `ArticleGrid`

--- src/components/ArticleGrid/ArticleCardHighlighted/index.jsx
+++ src/components/ArticleGrid/ArticleCardHighlighted/index.jsx
@@ -82,7 +82,11 @@ const Author = styled.p`
 `
 
 const CallToAction = styled(Link)`
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
+    height: 100%;
     
     &:active{
         outline: none;
