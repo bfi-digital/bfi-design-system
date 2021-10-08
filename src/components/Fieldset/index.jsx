add8d76a3e761afb1c70977e2744921b8fcfe644
Richard Howell <richardh@deeson.co.uk>
2021-10-08 09:47:44 +0100

    Add fieldset component

---
+++ src/components/Fieldset/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import theme from "../_theme";
+
+const Outer = styled.fieldset`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  border: 0;
+
+  @media screen and (min-width: ${theme.l}) {
+    margin: 0 0 25px;
+  }
+`;
+
+export const Fieldset = ({ children }) => <Outer>{children}</Outer>;
