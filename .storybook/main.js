980b5e7c89e011effbc61227eb262707413d4892
Paul Kiddle <paul.kiddle@bfi.org.uk>
2021-11-23 15:29:39 +0000

    Upgrade rollup & plugins

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 const path = require("path")
 
 module.exports = {
-  stories: ["../src/**/stories.(tsx|jsx)", "../src/**/*.stories.mdx"],
+  stories: ["../src/**/stories.@(tsx|jsx)", "../src/**/*.stories.mdx"],
   addons: [
         "@storybook/addon-a11y",
         "@storybook/addon-docs",
-        "@storybook/addon-viewport/register",
+        "@storybook/addon-viewport",
         "@storybook/addon-backgrounds",
         {
           name: "@storybook/addon-storysource",
