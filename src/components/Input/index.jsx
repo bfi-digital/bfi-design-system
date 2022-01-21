d316b2e628bdf200cee719c9ce6aece7789dcdcd
Paul Kiddle <paul.kiddle@bfi.org.uk>
2022-01-21 14:33:55 +0000

    Pull form components from upc branch

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import theme from "../_theme";
+
+const Control = styled.div`
+  width: 100%;
+  
+  @media screen and (min-width: ${theme.l}) {
+    max-width: 416px;
+  }
+
+  .FormField-group & {
+    @media screen and (max-width: ${theme.l}) {
+      margin-bottom: 1px;
+    }
+  }
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  font-weight: ${theme.fontWeight_semiBold};
+  width: 100%;
+`;
+
+const InputEl = styled.input`
+  display: block;
+  margin-top: 5px;
+  padding: 13px 10px;
+  padding-top: 12px;
+  font-size: ${theme.fontSize_s};
+  font-weight: ${theme.fontWeight_semiBold};
+  border: 2px solid ${theme.black};
+  background-color: ${theme.white};
+
+  border-bottom: 5px solid ${theme.primary};
+  transition: border 0.3s;
+  width: 100%;
+
+  &:hover {
+    border-bottom: 5px solid ${theme.lightFocus};
+  }
+
+  &:focus {
+    border: 2px solid ${theme.lightFocus};
+    border-bottom: 5px solid ${theme.focus};
+    outline: none;
+  }
+
+  ::placeholder,
+  ::-webkit-input-placeholder {
+    font-size: ${theme.fontSize_s};
+    font-weight: ${theme.fontWeight_semiBold};
+    color: ${theme.black};
+  }
+
+  :-ms-input-placeholder {
+    font-size: ${theme.fontSize_s};
+    font-weight: ${theme.fontWeight_semiBold};
+    color: ${theme.black};
+  }
+
+  &[disabled] {
+    background-color: ${theme.white};
+    border-color: ${theme.black};
+
+    &::placeholder,
+    ::placeholder,
+    &::-webkit-input-placeholder,
+    ::-webkit-input-placeholder {
+      font-weight: ${theme.fontWeight_semiBold};
+      color: ${theme.black};
+    }
+  }
+  
+  .FormField-group & {
+    @media screen and (min-width: ${theme.l}) {
+      border-right: 0;
+    }
+  }
+`;
+
+export const Input = ({
+  label,
+  required,
+  disabled,
+  autoComplete,
+  title,
+  ariaLabel,
+  id,
+  maxLength,
+  name,
+  type,
+  value,
+  placeholder,
+  style,
+  onChange,
+}) => (
+  <Control>
+    <Label>
+      {label}
+      <InputEl
+        required={required}
+        disabled={disabled}
+        autoComplete={autoComplete}
+        title={title}
+        aria-label={ariaLabel}
+        id={id}
+        maxLength={maxLength}
+        name={name}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        style={style}
+        onChange={onChange}
+      ></InputEl>
+    </Label>
+  </Control>
+);
+
+Input.propTypes = {
+  required: PropTypes.bool,
+  disabled: PropTypes.bool,
+  autoComplete: PropTypes.string,
+  title: PropTypes.string,
+  ariaLabel: PropTypes.string,
+  className: PropTypes.string,
+  id: PropTypes.string,
+  maxLength: PropTypes.number,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+Input.defaultProps = {
+  label: "",
+  id: "",
+  name: "",
+  heading: "",
+  text: "",
+  email: "",
+  value: "",
+};
---
+++ src/components/Selectbox/index.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import theme from "../_theme";
+
+const Outer = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Label = styled.label`
+  flex-basis: 100%;
+  margin-bottom: 5px;
+  font-size: 16px;
+  font-weight: ${theme.fontWeight_semiBold};
+`;
+
+const Control = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  
+  @media screen and (min-width: ${theme.l}) {
+    max-width: 416px;
+  }
+
+  &:focus-within {
+    &:before {
+      background-color: ${theme.focus};
+    }
+  }
+
+  &:before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 5px;
+    position: absolute;
+    top: auto;
+    right: auto;
+    bottom: 0;
+    left: 0px;
+    z-index: 1;
+    background-color: ${theme.primary};
+    transition: background-color 0.25s, height 0.25s;
+    transform-origin: bottom;
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    bottom: auto;
+    left: auto;
+    display: block;
+    width: 0;
+    height: 0;
+    transform: translateY(-50%);
+    border-left: 11px solid transparent;
+    border-right: 11px solid transparent;
+    border-top: 15px solid ${theme.black};
+    pointer-events: none;
+  }
+`;
+
+const Select = styled.select`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  flex-basis: 100%;
+  padding: 14px 12px 18px;
+  background-color: ${theme.white};
+  border: 2px solid ${theme.black};
+  border-radius: 0;
+  padding-right: 62px;
+  font-size: 17px;
+  font-weight: ${theme.fontWeight_semiBold};
+
+  &:hover {
+    border: 2px solid ${theme.lightFocus};
+  }
+
+  &:active,
+  &:focus {
+    outline: 0;
+    border-radius: 0;
+    border: 2px solid ${theme.lightFocus};
+  }
+`;
+
+const Option = styled.option``;
+
+export const Selectbox = ({ id, placeholder, items, label, value, onChange }) => (
+  <Outer>
+    <Label htmlFor={id}>{label}</Label>
+    <Control>
+      <Select id={id} onChange={onChange} value={value}>
+        {placeholder && <Option value="">{placeholder}</Option>}
+        {items.map((item) => (
+          <Option key={item.value} value={item.value}>{item.label}</Option>
+        ))}
+      </Select>
+    </Control>
+  </Outer>
+);
+
+Selectbox.propTypes = {
+  items: PropTypes.array,
+  label: PropTypes.string,
+  value: PropTypes.string,
+  id: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+Selectbox.defaultProps = {
+  items: [],
+  label: "",
+};
