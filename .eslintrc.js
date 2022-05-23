f9ceeb1ae24f1fcb1bdb27fd5b1e168e38928cb6
Paul Kiddle <paul.kiddle@bfi.org.uk>
2022-05-23 11:15:07 +0100

    Upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,33 @@
 module.exports = {
-	"env": {
-		"browser": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:jsx-a11y/recommended"
-	],
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
-	},
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true
-		},
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"react",
-		"jsx-a11y"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			4
-		],
-		// "linebreak-style": [
-		// 	"error",
-		// 	"unix"
-		// ],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"never"
-		],
-		"react/prop-types": 0
+  "env": {
+    "browser": true
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:jsx-a11y/recommended", "plugin:storybook/recommended"],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-	}
-}
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["react", "jsx-a11y"],
+  "rules": {
+    "indent": ["error", 4],
+    // "linebreak-style": [
+    // 	"error",
+    // 	"unix"
+    // ],
+    "quotes": ["error", "double"],
+    "semi": ["error", "never"],
+    "react/prop-types": 0
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+};
