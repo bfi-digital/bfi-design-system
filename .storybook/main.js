15168d66eb63eb97b945dabc9e4612dc438f1d8d
Wayne Parkes <wayne.parkes@gmail.com>
2022-08-07 22:28:40 +0100

    New Dialog component to replace NPM dep, added controls and Figma to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,23 @@
-const path = require("path")
-
 module.exports = {
-  stories: ["../src/**/stories.@(tsx|jsx)", "../src/**/*.stories.mdx"],
+  // glob pattern matches: story|prefixed.story|stories|prefixed.stories .jsx|.tsx|.mdx
+  stories: ["../src/**/*stor{y,ies}.{[jt]s,md}x"],
   addons: [
     "@storybook/addon-a11y",
+    "@storybook/addon-controls",
     "@storybook/addon-docs",
     "@storybook/addon-viewport",
     "@storybook/addon-backgrounds",
+    "storybook-addon-designs",
     {
       name: "@storybook/addon-storysource",
       options: {
         rule: {
-          test: [/stories\.jsx?$/],
-        },
-      },
+          test: [/stories\.jsx?$/]
+        }
+      }
     }
   ],
   features: {
     previewMdx2: true
   }
-};
+}
